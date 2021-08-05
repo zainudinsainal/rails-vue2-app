5461bb97401c3e8c8016fdb7a4528b391998720d
zainudinsainal <supernova37@gmail.com>
2021-08-05 18:44:44 +0800

    Integrate Vuetify

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
+import vuetify from '@/plugins/vuetify'
 
 export default () => {
   document.addEventListener('turbolinks:load', () => {
     new Vue({
       router,
       store,
+      vuetify,
       render: h => h(App)
     }).$mount('#vue-app')
   })
